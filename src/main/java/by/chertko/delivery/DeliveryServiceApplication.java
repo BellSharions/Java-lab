b8442d8b7f0ccfe8047f7f013a16d872686525e2
BellSharions <64787021+BellSharions@users.noreply.github.com>
2021-11-24 08:37:55 +0300

    Update DeliveryServiceApplication.java

--- src/main/java/by/chertko/delivery/DeliveryServiceApplication.java
+++ src/main/java/by/chertko/delivery/DeliveryServiceApplication.java
@@ -1,9 +1,12 @@
 package by.chertko.delivery;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =  @Info(title = "Delivery API"))
 public class DeliveryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeliveryServiceApplication.class, args);
